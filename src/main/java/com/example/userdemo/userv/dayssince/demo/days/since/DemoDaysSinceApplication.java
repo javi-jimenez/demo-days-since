3bf8a5f9a8b7fc725be64ea223fc6f1c34de9af1
Javier Jiménez <javi-jimenez@users.noreply.github.com>
2025-02-24 12:30:51 +0100

    feat: add microservice calculating days since

---
+++ src/main/java/com/example/userdemo/userv/dayssince/demo/days/since/DemoDaysSinceApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdemo.userv.dayssince.demo.days.since;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDaysSinceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDaysSinceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userdemo/userv/dayssince/demo/days/since/DemoDaysSinceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdemo.userv.dayssince.demo.days.since;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDaysSinceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
